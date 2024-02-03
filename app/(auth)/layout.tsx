229e74ed62ab758b2fef15c4e7760adcbe94e0d6
Bruno Hirae <brunodeluca117@outlook.com>
2024-02-03 15:57:38 -0300

    chore: update folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+	return <div className='bg-red-500 h-full'>{children}</div>;
+};
+
+export default Layout;
