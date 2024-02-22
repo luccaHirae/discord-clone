203be22c181bdad9e92e0b9b79698c33804803ce
Bruno Hirae <brunodeluca117@outlook.com>
2024-02-22 11:46:35 -0300

    feat: add create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ModalProvider } from '@/components/providers/modal-provider';
 import { cn } from '@/lib/utils';
 import './globals.css';
 
@@ -27,6 +28,7 @@ export default function RootLayout({
 						storageKey='discord-clone-theme'
 						enableSystem={false}
 					>
+						<ModalProvider />
 						{children}
 					</ThemeProvider>
 				</body>
