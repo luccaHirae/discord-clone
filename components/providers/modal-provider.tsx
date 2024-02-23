6ab629a680c0703619c9bfad9db053b5247f4122
Bruno Hirae <brunodeluca117@outlook.com>
2024-02-23 14:22:21 -0300

    feat: add channel creation modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 import { InviteModal } from '@/components/modals/invite-modal';
 import { EditServerModal } from '@/components/modals/edit-server-modal';
 import { MembersModal } from '@/components/modals/members-modal';
+import { CreateChannelModal } from '@/components/modals/create-channel-modal';
 
 export const ModalProvider = () => {
 	const [isMounted, setIsMounted] = useState(false);
@@ -23,6 +25,7 @@ export const ModalProvider = () => {
 			<InviteModal />
 			<EditServerModal />
 			<MembersModal />
+			<CreateChannelModal />
 		</>
 	);
 };
