6f6eab9fa670b5cd228555a0053cdc222e6328da
Bruno Hirae <brunodeluca117@outlook.com>
2024-02-26 22:10:33 -0300

    feat: add chat header component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,49 @@
-export default function Page() {
+import { redirect } from 'next/navigation';
+import { redirectToSignIn } from '@clerk/nextjs';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { ChatHeader } from '@/components/chat/chat-header';
+
+interface Props {
+	params: {
+		serverId: string;
+		channelId: string;
+	};
+}
+
+export default async function Page({ params }: Props) {
+	const profile = await currentProfile();
+
+	if (!profile) {
+		return redirectToSignIn();
+	}
+
+	const [channel, member] = await Promise.all([
+		db.channel.findUnique({
+			where: {
+				id: params.channelId,
+			},
+		}),
+		db.member.findFirst({
+			where: {
+				serverId: params.serverId,
+				profileId: profile.id,
+			},
+		}),
+	]);
+
+	if (!channel || !member) {
+		redirect('/');
+	}
+
 	return (
-		<div>
-			<h1>Page</h1>
+		<div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+			<ChatHeader
+				name={channel.name}
+				serverId={channel.serverId}
+				type='channel'
+			/>
 		</div>
 	);
 }
