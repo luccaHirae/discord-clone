bcf1bd8c7a7a9f71aa3bc55b25af32336b231643
Bruno Hirae <brunodeluca117@outlook.com>
2024-02-26 21:41:49 -0300

    feat: add channel id page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,3 +1,48 @@
-export default function Page() {
-	return <div>Server Page</div>;
+import { redirect } from 'next/navigation';
+import { redirectToSignIn } from '@clerk/nextjs';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+
+interface Props {
+	params: {
+		serverId: string;
+	};
+}
+
+export default async function Page({ params }: Props) {
+	const profile = await currentProfile();
+
+	if (!profile) {
+		return redirectToSignIn();
+	}
+
+	const server = await db.server.findUnique({
+		where: {
+			id: params.serverId,
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+		include: {
+			channels: {
+				where: {
+					name: 'general',
+				},
+				orderBy: {
+					createdAt: 'asc',
+				},
+			},
+		},
+	});
+
+	const initialChannel = server?.channels[0];
+
+	if (initialChannel?.name !== 'general') {
+		return null;
+	}
+
+	return redirect(`/servers/${params.serverId}/channels/${initialChannel.id}`);
 }
