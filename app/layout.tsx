42b5e8d687a03cd900e0d094096bb758faa6ddc1
Bruno Hirae <brunodeluca117@outlook.com>
2024-02-28 14:14:05 -0300

    feat: add chat messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
 import { SocketProvider } from '@/components/providers/socket-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 import { cn } from '@/lib/utils';
+
 import './globals.css';
 
 const open_sans = Open_Sans({ subsets: ['latin'] });
@@ -32,7 +34,7 @@ export default function RootLayout({
 					>
 						<SocketProvider>
 							<ModalProvider />
-							{children}
+							<QueryProvider>{children}</QueryProvider>
 						</SocketProvider>
 					</ThemeProvider>
 				</body>
